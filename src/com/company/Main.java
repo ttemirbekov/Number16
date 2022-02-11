57c6628fdacbced6a9896384800b3d6d722943dc
turatbek <temirbekov.tt@gmail.com>
2022-02-11 20:32:39 +0600

    made correction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Enter Radius of Circle: ");
         Scanner scanner = new Scanner(System.in);
-        int radius = scanner.nextInt();
+        double radius = scanner.nextInt();
 
         Circle circle = new Circle();
         circle.circumference(radius);
